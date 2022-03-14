6f4091443015450d34c0711912604e75d9514e42
Nishant Aklecha <nishant.aklecha@gmail.com>
2022-03-15 00:39:34 +0530

    Frontend

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ui';
+
+  constructor(public http : HttpClient) {
+    
+  }
+}
